670b3c7354e5b0547720d4019f93e865e1850c0f
Jan Skvaril <kondr4@seznam.cz>
2024-05-09 17:49:35 +0200

    Updated example

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-import AEDescriptor
+from AEDescriptor import *
 
 
-# img = cv.imread("test_image.png", cv.IMREAD_GRAYSCALE)
-# descriptor = AEDescriptor(GetModelName(LossFunction.FFT, 16))
-# print(descriptor.Eval(img))
+img = cv.imread("test_img.png", cv.IMREAD_GRAYSCALE)
+descriptor = AEDescriptor(GetModelName(LossFunction.FFT, 16))
+print(descriptor.Eval(img))
